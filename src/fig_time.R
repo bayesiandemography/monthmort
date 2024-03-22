26aeb518602571d6c123841ade6f50bcc66c0e62
John Bryant <john.r.bryant@gmail.com>
2024-03-23 09:06:14 +1300

    updated to use new version of bage

--- src/fig_time.R
+++ src/fig_time.R
@@ -1,41 +1,34 @@
 
-
 library(bage)
 library(dplyr, warn.conflicts = FALSE)
 library(command)
-library(rvec)
+library(rvec, warn.conflicts = FALSE)
 library(poputils)
 library(tidyr, warn.conflicts = FALSE)
 library(ggplot2)
-library(lubridate)
+library(lubridate, warn.conflicts = FALSE)
 
 cmd_assign(mod = "out/mod.rds",
+           col_fill = "steelblue1",
+           col_line = "black",
            .out = "out/fig_time.pdf")
-
-col_fill <- "#FF7F50"
-col_line <- "#2F4F4F"
 
 comp <- components(mod)
 
-trend <- comp %>%
-    filter(component == "effect",
-           term == "time") %>%
-    mutate(component = "trend") %>%
-    select(-term)
-cyclical <- comp %>%
-    filter(component == "cyclical",
-           term == "effect") %>%
-    select(-term)
-    
-data <- bind_rows(trend, cyclical) %>%
-    pivot_wider(names_from = component, values_from = .fitted) %>%
-    mutate(effect = trend + cyclical) %>%
-    pivot_longer(c(effect, trend, cyclical), names_to = "component", values_to = ".fitted") %>%
-    mutate(component = factor(component,
-                              levels = c("effect", "trend", "cyclical"),
-                              labels = c("Time Effect", "Trend Component", "Cyclical Component"))) %>%
-    mutate(level = as.Date(level)) %>%
-    mutate(draws_ci(.fitted))
+data <- comp %>%
+  filter(term == "time",
+         component != "hyper") %>%
+  mutate(component = factor(component,
+                            levels = c("effect", "trend", "cyclical"),
+                            labels = c("Time Effect", "Trend Component", "Cyclical Component"))) %>%
+  mutate(level = as.Date(level)) %>%
+  mutate(draws_ci(.fitted))
+
+y_abs_max <- data %>%
+  select(.fitted.lower, .fitted.upper) %>%
+  unlist() %>%
+  abs() %>%
+  max()
 
 p <- ggplot(data,
        aes(x = level,
@@ -50,7 +43,7 @@ p <- ggplot(data,
               linewidth = 0.25) +
     scale_x_date(breaks = "1 year",
                  date_labels = "%Y") +
-    ylim(-0.15, 0.15) +
+    ylim(-y_abs_max, y_abs_max) +
     ylab("") +
     xlab("") +
     theme(text = element_text(size = 18))
