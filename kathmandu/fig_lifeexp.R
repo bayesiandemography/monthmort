6766f2b0b0d87c2d3c7bfbea62f70b0f35e10b53
John Bryant <john.r.bryant@gmail.com>
2024-11-19 06:38:49 +0700

    working on poster

--- kathmandu/fig_lifeexp.R
+++ kathmandu/fig_lifeexp.R
@@ -27,6 +27,8 @@ p <- ggplot(data, aes(x = time)) +
   geom_line(aes(y = modelled.mid,
                 color = sex),
             linewidth = 0.2) +
+  scale_x_date(date_minor_breaks = "1 year") +
+  scale_y_continuous(minor_breaks = 2) +
   scale_fill_manual(values = c("darkgreen", "darkorange")) +
   scale_color_manual(values = c("darkgreen", "darkorange")) +
   ylab("") +
