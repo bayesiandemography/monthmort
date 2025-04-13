9284bc8f40c992bbc364e11ecdd9a187043fc205
John Bryant <john.r.bryant@gmail.com>
2025-04-13 07:53:48 +0700

    added excess_agesex

---
+++ src/fig_paper_excess_agesex.R
@@ -0,0 +1,43 @@
+
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(rvec)
+  library(poputils)
+  library(lubridate)
+  library(ggplot2)
+  library(command)
+})
+
+cmd_assign(.excess = "out/excess.rds",
+           sex = "Female",
+           col_fill = "#A6CEE3",
+           col_line = "#1F4E79",
+           .out = "out/fig_paper_excess_agesex_female.pdf")
+
+excess <- readRDS(.excess)
+
+data <- excess |>
+  filter(sex == !!sex) |>
+  mutate(excess = excess / 1000) |>
+  mutate(draws_ci(excess))
+
+p <- ggplot(data, aes(x = time)) +
+  facet_wrap(vars(age)) +
+  geom_ribbon(aes(ymin = excess.lower,
+                  ymax = excess.upper),
+              fill = col_fill) +
+  geom_line(aes(y = excess.mid),
+            color = col_line,
+            linewidth = 0.25) +
+  geom_hline(yintercept = 0, linewidth = 0.25) +
+  ylab("Deaths (000)") +
+  xlab("")
+
+graphics.off()
+pdf(file = .out,
+    width = 6,
+    height = 6.5)
+plot(p)
+dev.off()        
+
+
