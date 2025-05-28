0f889e304b65d12681bdab90ab5e85f81e7020a0
John Bryant <john.r.bryant@gmail.com>
2025-05-28 13:42:28 +0700

    working on graphs for data section

--- src/fig_pc_change.R
+++ src/fig_pc_change.R
@@ -12,13 +12,13 @@ pc_change <- readRDS(.pc_change)
 p <- ggplot(pc_change, aes(x = 0, xend = pc_change, y = age)) +
   facet_grid(vars(sex), vars(series)) +
   geom_segment(linewidth = 0.3,
-               arrow = arrow(length = unit(0.15,"cm"))) +
+               arrow = arrow(length = unit(0.1,"cm"))) +
   xlab("Percent") +
-  ylab("") +
+  ylab("Age") +
   theme(text = element_text(size = 10))
 
 pdf(file = .out,
-    width = 5,
+    width = 6,
     height = 3)
 plot(p)
 dev.off()        
