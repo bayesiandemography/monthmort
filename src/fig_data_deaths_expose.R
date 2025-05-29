ec1b537d25536552dcd87c30f4c436f63657b47a
John Bryant <john.r.bryant@gmail.com>
2025-05-29 17:01:59 +0700

    tidying fig_data_deaths_expose

--- src/fig_data_deaths_expose.R
+++ src/fig_data_deaths_expose.R
@@ -1,7 +1,8 @@
 
 suppressPackageStartupMessages({
+  library(dplyr)
   library(ggplot2)
-  library(poputils)
+  library(patchwork)
   library(command)
 })
 
@@ -10,18 +11,43 @@ cmd_assign(.data_deaths_expose = "out/data_deaths_expose.rds",
 
 data_deaths_expose <- readRDS(.data_deaths_expose)
 
-p <- ggplot(data_deaths_expose,
-            aes(x = age_mid, y = value, color = factor(time))) +
-  facet_grid(vars(series), vars(sex), scale = "free_y") +
+p_deaths <- data_deaths_expose |>
+  filter(series == "Deaths") |>
+  ggplot(aes(x = age_mid, y = value, color = factor(time))) +
+  facet_grid(vars(sex), vars(series)) +
   geom_line() +
   scale_color_manual(values = c("#63a3cc", "#08306b")) +
   xlab("Age") +
   ylab("") +
-  theme(legend.title = element_blank())
+  theme(legend.position = "none")
+
+p_expose <- data_deaths_expose |>
+  filter(series == "Exposure") |>
+  ggplot(aes(x = age_mid, y = value, color = factor(time))) +
+  facet_grid(vars(sex), vars(series)) +
+  geom_line() +
+  scale_color_manual(values = c("#63a3cc", "#08306b")) +
+  xlab("Age") +
+  ylab("") +
+  theme(legend.position = "top",
+        legend.title = element_blank())
+
+p_lograte <- data_deaths_expose |>
+  filter(series == "Log Rate") |>
+  ggplot(aes(x = age_mid, y = value, color = factor(time))) +
+  facet_grid(vars(sex), vars(series)) +
+  geom_line() +
+  scale_color_manual(values = c("#63a3cc", "#08306b")) +
+  xlab("Age") +
+  ylab("") +
+  theme(legend.position = "none")
+
+
+p <- p_deaths + p_expose + p_lograte
 
 pdf(file = .out,
-    width = 5,
-    height = 5)
+    width = 6,
+    height = 4)
 plot(p)
 dev.off()
 
