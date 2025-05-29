e647821775f091a819d1b8a330f85718ea8c9d14
John Bryant <john.r.bryant@gmail.com>
2025-05-29 18:03:10 +0700

    tweaked fig_data_monthly

--- src/fig_data_monthly.R
+++ src/fig_data_monthly.R
@@ -9,16 +9,26 @@ cmd_assign(.data_monthly = "out/data_monthly.rds",
 
 data_monthly <- readRDS(.data_monthly)
 
+breaks_fun <- function(limits) {
+  max <- limits[2]
+  if (max <= 12) {
+    ans <- seq(from = 0, to = max, by = 3)
+  } else {
+    ans <- scales::extended_breaks()(limits)
+  }
+  ans
+}
+
 p <- ggplot(data_monthly,
-            aes(x = time, y = rate)) +
+            aes(x = time, y = deaths)) +
   facet_grid(vars(age), vars(sex), scale = "free_y") +
   geom_point(size = 0.2) +
   scale_x_date(breaks = seq.Date(from = as.Date("2000-01-01"),
-                                 to = as.Date("2025-01-01"),
+                                 to = as.Date("2020-01-01"),
                                  by = "5 year"),
                date_minor_breaks = "1 year",
                date_labels = "%Y") +
-  scale_y_continuous(labels = function(x) format(x, scientific = FALSE),
+  scale_y_continuous(breaks = breaks_fun,
                      limits = c(0, NA)) +
   xlab("") +
   ylab("")
